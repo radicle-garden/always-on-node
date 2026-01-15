129f6a4b5c4ff67eb7a87af1aba3267b8955238b
Rūdolfs Ošiņš <rudolfs@osins.org>
2026-01-15 11:29:53 -0600

    Bring eslint.config.ts closer to stock sveltekit

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,8 +1,9 @@
 import prettier from "eslint-config-prettier";
 import svelte from "eslint-plugin-svelte";
+import { defineConfig } from "eslint/config";
 import globals from "globals";
 import { fileURLToPath } from "node:url";
-import tseslint from "typescript-eslint";
+import ts from "typescript-eslint";
 
 import { includeIgnoreFile } from "@eslint/compat";
 import js from "@eslint/js";
@@ -11,25 +12,32 @@ import svelteConfig from "./svelte.config.js";
 
 const gitignorePath = fileURLToPath(new URL("./.gitignore", import.meta.url));
 
-export default [
+export default defineConfig(
   includeIgnoreFile(gitignorePath),
   js.configs.recommended,
-  ...tseslint.configs.recommended,
+  ...ts.configs.recommended,
   ...svelte.configs.recommended,
   prettier,
   ...svelte.configs.prettier,
   {
-    languageOptions: {
-      globals: { ...globals.browser, ...globals.node },
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+
+    rules: {
+      // typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
+      // see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
+      "no-undef": "off",
     },
   },
   {
-    files: ["**/*.svelte", "**/*.svelte.js"],
+    files: ["**/*.svelte", "**/*.svelte.ts", "**/*.svelte.js"],
+
     languageOptions: {
       parserOptions: {
+        projectService: true,
+        extraFileExtensions: [".svelte"],
+        parser: ts.parser,
         svelteConfig,
-        parser: tseslint.parser,
       },
     },
   },
-];
+);
