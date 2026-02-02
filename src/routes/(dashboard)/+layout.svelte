d13c264a3d5c7859e3d67998e4023e6aa11101aa
Tom <contacttomnash@gmail.com>
2026-02-02 11:17:45 +0100

    Fix Header bottom border and margin

--- src/routes/(dashboard)/+layout.svelte
+++ src/routes/(dashboard)/+layout.svelte
@@ -48,7 +48,8 @@
   errorIcon={toasterErrorIcon} />
 
 <div class="flex min-h-screen flex-col items-center bg-surface-base pb-8">
-  <header class="min-h-16 w-full px-4 py-2">
+  <header
+    class="mb-0 min-h-11 w-full border-b border-border-subtle px-4 py-2 sm:mb-20">
     <Header user={data.user} {breadcrumbs} fqdn={data.fqdn} />
   </header>
   <main
