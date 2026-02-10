69dbaa6a912f47847155111e1c5fd2b70a0bf99a
Tom <contacttomnash@gmail.com>
2026-02-10 10:41:45 +0100

    Add error pages

---
+++ src/components/ErrorContainer.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import { resolve } from "$app/paths";
+  import Icon from "$components/Icon.svelte";
+  import { Button } from "$lib/components/ui/button";
+
+  let { status, primaryButtonText } = $props();
+</script>
+
+<div
+  class="flex min-h-[calc(100vh-15.5rem)] flex-col items-center justify-center gap-4 p-4">
+  <div class="txt-heading-xxl">
+    {status}
+  </div>
+  <div class="txt-body-l-regular text-center">
+    {#if status === 404}
+      Seems you've clicked a link that's not here anymore.
+    {:else}
+      Seems like the app is broken.
+    {/if}
+  </div>
+  <Button variant="primary" href={resolve("/")}>
+    {primaryButtonText}
+  </Button>
+  <Button variant="outline" href="https://radicle.zulipchat.com/">
+    Get help on Zulip
+    <Icon name="open-external" />
+  </Button>
+</div>
---
+++ src/routes/(dashboard)/+error.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  import { page } from "$app/state";
+  import ErrorContainer from "$components/ErrorContainer.svelte";
+</script>
+
+<ErrorContainer status={page.status} primaryButtonText="Back to dashboard" />
